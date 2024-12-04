5d483ffb7c02613c858b4cbedc4ca2c6c9c36e6d
tamarabush <tamara.abush14@outlook.com>
2024-12-04 11:00:47 -0500

    added data in BarChart.jsx

---
+++ src/components/BarChart.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ChartComponent from './ChartComponent';
+
+const BarChart = ({ data }) => {
+  const chartData = {
+    labels: data.months,
+    datasets: [
+      {
+        label: 'Monthly Sales',
+        data: data.sales,
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <ChartComponent type="bar" data={chartData} options={options} />;
+};
+
+export default BarChart;

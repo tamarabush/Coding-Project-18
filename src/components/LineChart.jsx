16558ec9d877bbff58ffec3d6be1ecfc5f75bb14
tamarabush <tamara.abush14@outlook.com>
2024-12-04 11:06:32 -0500

    added data in LineChart.jsx

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ChartComponent from './ChartComponent';
+
+const LineChart = ({ data }) => {
+  const chartData = {
+    labels: data.months,
+    datasets: [
+      {
+        label: 'Monthly Profits',
+        data: data.profits,
+        backgroundColor: 'rgba(153, 102, 255, 0.2)',
+        borderColor: 'rgba(153, 102, 255, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <ChartComponent type="line" data={chartData} options={options} />;
+};
+
+export default LineChart;

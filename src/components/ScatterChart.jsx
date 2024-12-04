7bec599eb69ad7f642729ce3db542185874d6517
tamarabush <tamara.abush14@outlook.com>
2024-12-04 11:07:10 -0500

    added data in ScatterChart.jsx

---
+++ src/components/ScatterChart.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ChartComponent from './ChartComponent';
+
+const ScatterChart = ({ data }) => {
+  const chartData = {
+    datasets: [
+      {
+        label: 'Expenses vs. Profits',
+        data: data.expenses.map((expense, index) => ({
+          x: expense,
+          y: data.profits[index],
+        })),
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      x: {
+        beginAtZero: true,
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <ChartComponent type="scatter" data={chartData} options={options} />;
+};
+
+export default ScatterChart;
